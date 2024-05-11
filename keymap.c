7d208f8fb13765580b69dea5e37d273d37f38ab7
godalming <r2hk9ahnf@relay.firefox.com>
2024-05-11 10:02:14 +0100

    add tab on an easier to use combo, remove the unused backspace word, and make the keyboard look nicer

--- keymap.c
+++ keymap.c
@@ -30,10 +30,10 @@ const uint16_t PROGMEM page_up[]   = {KC_D, KC_C, COMBO_END};
 const uint16_t PROGMEM end[]       = {KC_F, KC_V, COMBO_END};
 
 // Other key combos
-const uint16_t PROGMEM mouse1[]         = {SPACE, KC_G,   COMBO_END};
-const uint16_t PROGMEM mouse2[]         = {SPACE, KC_ENT, COMBO_END};
-const uint16_t PROGMEM backspace_word[] = {BSPC,  KC_T,   COMBO_END};
-const uint16_t PROGMEM delete[]         = {BSPC,  KC_ESC, COMBO_END};
+const uint16_t PROGMEM mouse1[] = {SPACE, KC_G,   COMBO_END};
+const uint16_t PROGMEM mouse2[] = {SPACE, KC_ENT, COMBO_END};
+const uint16_t PROGMEM tab[]    = {BSPC,  KC_T,   COMBO_END};
+const uint16_t PROGMEM delete[] = {BSPC,  KC_ESC, COMBO_END};
 
 // Define combos
 combo_t key_combos[] = {
@@ -57,35 +57,35 @@ combo_t key_combos[] = {
     COMBO(page_up,   KC_PGUP),
     COMBO(end,       KC_END),
 
-    COMBO(mouse1,         KC_BTN1),
-    COMBO(mouse2,         KC_BTN2),
-    COMBO(backspace_word, LCTL(KC_BSPC)),
-    COMBO(delete,         KC_DEL),
+    COMBO(mouse1, KC_BTN1),
+    COMBO(mouse2, KC_BTN2),
+    COMBO(tap,    KC_TAB),
+    COMBO(delete, KC_DEL),
 };
 
 // Keymap (flipped because my keyboard flips the keymap for some reason)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_split_3x6_3(
-  //.-----------------------------------------------------.                    .-----------------------------------------------------.
-      XXXXXXX,    KC_O,    KC_I,    KC_U,    KC_Y, XXXXXXX,                      XXXXXXX,    KC_R,    KC_E,    KC_W,   KC_Q,  XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_L,    KC_K,    KC_J,    KC_H, XXXXXXX,                      XXXXXXX,    KC_F,    KC_D,    KC_S,   KC_A,  XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_B,    KC_P,    KC_M,    KC_N, XXXXXXX,                      XXXXXXX,    KC_V,    KC_C,    KC_X,   KC_Z,  XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------.  .--------+--------+--------+--------+--------+--------+--------|
-                                           KC_ENT,   SPACE,    KC_G,       KC_T,    BSPC,  KC_ESC
-  //                                    `--------------------------`  `--------------------------`
+  [0] = LAYOUT_split_3x6_3( 
+  // ╭────────┬────────┬────────┬────────┬────────┬────────╮                    ╭────────┬────────┬────────┬────────┬────────┬────────╮
+       XXXXXXX,    KC_O,    KC_I,    KC_U,    KC_Y, XXXXXXX,                      XXXXXXX,    KC_R,    KC_E,    KC_W,   KC_Q,  XXXXXXX,
+  // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
+       XXXXXXX,    KC_L,    KC_K,    KC_J,    KC_H, XXXXXXX,                      XXXXXXX,    KC_F,    KC_D,    KC_S,   KC_A,  XXXXXXX,
+  // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
+       XXXXXXX,    KC_B,    KC_P,    KC_M,    KC_N, XXXXXXX,                      XXXXXXX,    KC_V,    KC_C,    KC_X,   KC_Z,  XXXXXXX,
+  // ╰────────┴────────┴────────┴────────┼────────┼────────┼────────╮  ╭────────┼────────┼────────┼────────┴────────┴────────┴────────╯
+                                            KC_ENT,   SPACE,    KC_G,       KC_T,    BSPC,  KC_ESC
+  //                                     ╰────────┴────────┴────────╯  ╰────────┴────────┴────────╯
   ),
 
   [1] = LAYOUT_split_3x6_3(
-  //.-----------------------------------------------------.                    .-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, KC_BSLS, KC_QUOT, KC_SCLN, XXXXXXX,                      XXXXXXX,  KC_EQL, KC_MINS,  KC_GRV, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_0,    KC_9,    KC_8,    KC_7, XXXXXXX,                      XXXXXXX,    KC_4,    KC_3,    KC_2,    KC_1, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_SLSH,  KC_DOT, KC_COMM,    KC_6, XXXXXXX,                      XXXXXXX,    KC_5, KC_RBRC, KC_LBRC,   KC_F7, XXXXXXX, // F7 is the key that I use to enter my BIOS
-  //|--------+--------+--------+--------+--------+--------+--------.  .--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______,  KC_TAB,    XXXXXXX, XXXXXXX, _______
-  //                                    `--------------------------`  `--------------------------`
+  // ╭────────┬────────┬────────┬────────┬────────┬────────╮                    ╭────────┬────────┬────────┬────────┬────────┬────────╮
+       XXXXXXX, XXXXXXX, KC_BSLS, KC_QUOT, KC_SCLN, XXXXXXX,                      XXXXXXX,  KC_EQL, KC_MINS,  KC_GRV, XXXXXXX, XXXXXXX,
+  // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
+       XXXXXXX,    KC_0,    KC_9,    KC_8,    KC_7, XXXXXXX,                      XXXXXXX,    KC_4,    KC_3,    KC_2,    KC_1, XXXXXXX,
+  // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
+       XXXXXXX, KC_SLSH,  KC_DOT, KC_COMM,    KC_6, XXXXXXX,                      XXXXXXX,    KC_5, KC_RBRC, KC_LBRC,   KC_F7, XXXXXXX, // F7 is the key that I use to enter my BIOS
+  // ╰────────┴────────┴────────┴────────┼────────┼────────┼────────╮  ╭────────┼────────┼────────┼────────┴────────┴────────┴────────╯
+                                           _______, _______, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
+  //                                     ╰────────┴────────┴────────╯  ╰────────┴────────┴────────╯
   ),
 };
